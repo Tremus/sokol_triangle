d81fd91776a3bce2d7c8dad9dbf79191d10c05e2
Trémus <19359682+Tremus@users.noreply.github.com>
2024-10-13 16:40:57 +1000

    first draft program_offscreen_render.c

--- src/main.c
+++ src/main.c
@@ -4,10 +4,33 @@
 #include "sokol_gfx.h"
 #include "sokol_glue.h"
 
+static void my_logger(
+    const char* tag,              // always "sapp"
+    uint32_t    log_level,        // 0=panic, 1=error, 2=warning, 3=info
+    uint32_t    log_item_id,      // SAPP_LOGITEM_*
+    const char* message_or_null,  // a message string, may be nullptr in release mode
+    uint32_t    line_nr,          // line number in sokol_app.h
+    const char* filename_or_null, // source filename, may be nullptr in release mode
+    void*       user_data)
+{
+    static char* LOG_LEVEL[] = {
+        "PANIC",
+        "ERROR",
+        "WARNING",
+        "INFO",
+    };
+    SOKOL_ASSERT(log_level > 0);
+    SOKOL_ASSERT(log_level < ARRLEN(LOG_LEVEL));
+    if (! message_or_null)
+        message_or_null = "";
+    print("[%s] %s %u:%s", LOG_LEVEL[log_level], message_or_null, line_nr, filename_or_null);
+}
+
 static void init(void)
 {
     sg_setup(&(sg_desc){
         .environment = sglue_environment(),
+        .logger.func = my_logger,
     });
 
     program_setup();
@@ -34,5 +57,5 @@ sapp_desc sokol_main(int argc, char* argv[])
         .height             = APP_HEIGHT,
         .window_title       = "GFX (sokol-app)",
         .icon.sokol_default = true,
-    };
+        .logger.func        = my_logger};
 }
---
+++ src/program_offscreen_render.c
@@ -0,0 +1,155 @@
+#include "common.h"
+
+#include "sokol_gfx.h"
+#include "sokol_glue.h"
+
+#include "program_offscreen_render.h"
+
+// application state
+static struct
+{
+    struct
+    {
+        sg_pass     pass;
+        sg_pipeline pip;
+        sg_bindings bind;
+    } offscreen;
+
+    struct
+    {
+        sg_pass_action pass_action;
+        sg_pipeline    pip;
+        sg_bindings    bind;
+    } display;
+} state;
+
+void program_setup()
+{
+    // offscreen
+    {
+        sg_image_desc img_desc = {
+            .render_target = true,
+            .width         = APP_WIDTH,
+            .height        = APP_HEIGHT,
+            .sample_count  = 1,
+            .pixel_format  = SG_PIXELFORMAT_RGBA8,
+            .label         = "offscreen-image"};
+        const sg_image col_img = sg_make_image(&img_desc);
+
+        img_desc.pixel_format = SG_PIXELFORMAT_DEPTH;
+        img_desc.label        = "depth-image";
+        sg_image depth_img    = sg_make_image(&img_desc);
+
+        state.offscreen.pass = (sg_pass){
+            .attachments = sg_make_attachments(&(sg_attachments_desc){
+                .colors[0].image     = col_img,
+                .depth_stencil.image = depth_img,
+                .label               = "offscreen-attachment"}),
+            .action      = {.colors[0] = {.load_action = SG_LOADACTION_CLEAR, .clear_value = {0.0f, 0.0f, 0.0f, 1.0f}}},
+            .label       = "offscreen-pass"};
+
+        // a vertex buffer with 3 vertices
+        // clang-format off
+        float vertices[] = {
+            // positions      // colors
+             0.0f,  0.5f,     1.0f, 0.0f, 0.0f, 1.0f,
+             0.5f, -0.5f,     0.0f, 1.0f, 0.0f, 1.0f,
+            -0.5f, -0.5f,     0.0f, 0.0f, 1.0f, 1.0f
+        };
+        // clang-format on
+        state.offscreen.bind.vertex_buffers[0] =
+            sg_make_buffer(&(sg_buffer_desc){.data = SG_RANGE(vertices), .label = "offscreen-vertices"});
+
+        // create shader from code-generated sg_shader_desc
+        sg_shader shd = sg_make_shader(offscreen_shader_desc(sg_query_backend()));
+
+        // create a pipeline object (default render states are fine for triangle)
+        state.offscreen.pip = sg_make_pipeline(
+            &(sg_pipeline_desc){// if the vertex layout doesn't have gaps, don't need to provide strides and offsets
+                                .layout =
+                                    {.attrs =
+                                         {[ATTR_offscreen_vs_position].format = SG_VERTEXFORMAT_FLOAT2,
+                                          [ATTR_offscreen_vs_color0].format   = SG_VERTEXFORMAT_FLOAT4}},
+                                .shader = shd,
+                                .depth =
+                                    {
+                                        .pixel_format  = SG_PIXELFORMAT_DEPTH,
+                                        .compare       = SG_COMPAREFUNC_LESS_EQUAL,
+                                        .write_enabled = true,
+                                    },
+                                // .sample_count = 1,
+                                .colors[0].pixel_format = SG_PIXELFORMAT_RGBA8,
+                                .label                  = "offscreen-pipeline"});
+    }
+
+    // display
+    {
+        // default pass action
+        state.display.pass_action = (sg_pass_action){
+            .colors[0] = {.load_action = SG_LOADACTION_CLEAR, .clear_value = {0.0f, 0.0f, 0.0f, 1.0f}}};
+
+        typedef struct
+        {
+            float   x, y;
+            int16_t u, v;
+        } vertex_t;
+
+        // a vertex buffer
+        // clang-format off
+        vertex_t vertices[] = {
+            {-1.0f,  1.0f, 0,     32767},
+            { 1.0f,  1.0f, 32767, 32767},
+            { 1.0f, -1.0f, 32767, 0},
+            {-1.0f, -1.0f, 0,     0},
+        };
+        // an index buffer with 2 triangles
+        uint16_t indices[] = {
+            0, 1, 2,
+            0, 2, 3,
+        };
+        // clang-format on
+        state.display.bind.vertex_buffers[0] =
+            sg_make_buffer(&(sg_buffer_desc){.data = SG_RANGE(vertices), .label = "quad-vertices"});
+
+        state.display.bind.index_buffer = sg_make_buffer(
+            &(sg_buffer_desc){.type = SG_BUFFERTYPE_INDEXBUFFER, .data = SG_RANGE(indices), .label = "quad-indices"});
+
+        // a shader (use separate shader sources here
+        sg_shader shd = sg_make_shader(display_shader_desc(sg_query_backend()));
+
+        // a pipeline state object
+        state.display.pip = sg_make_pipeline(&(sg_pipeline_desc){
+            .shader     = shd,
+            .index_type = SG_INDEXTYPE_UINT16,
+            .layout =
+                {.attrs =
+                     {[ATTR_display_vs_position].format  = SG_VERTEXFORMAT_FLOAT2,
+                      [ATTR_display_vs_texcoord0].format = SG_VERTEXFORMAT_SHORT2N}},
+            .label = "quad-pipeline"});
+
+        state.display.bind.fs.images[SLOT_tex] = sg_alloc_image();
+
+        // a sampler object
+        state.display.bind.fs.samplers[SLOT_smp] = sg_make_sampler(&(sg_sampler_desc){
+            .min_filter = SG_FILTER_LINEAR,
+            .mag_filter = SG_FILTER_LINEAR,
+        });
+    }
+}
+
+void program_tick()
+{
+    // offscreen
+    sg_begin_pass(&state.offscreen.pass);
+    sg_apply_pipeline(state.offscreen.pip);
+    sg_apply_bindings(&state.offscreen.bind);
+    sg_draw(0, 3, 1);
+    sg_end_pass();
+
+    // main
+    sg_begin_pass(&(sg_pass){.action = state.display.pass_action, .swapchain = sglue_swapchain()});
+    sg_apply_pipeline(state.display.pip);
+    sg_apply_bindings(&state.display.bind);
+    sg_draw(0, 6, 1);
+    sg_end_pass();
+}
