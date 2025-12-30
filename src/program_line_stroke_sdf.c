13fa67fe9ced7ef4dd0b0fa6018a24434296e7f1
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2025-12-30 13:05:30 +1000

    Add stroked path, somewhat anti aliased

---
+++ src/program_line_stroke_sdf.c
@@ -0,0 +1,115 @@
+#include "common.h"
+
+#include "program_line_stroke_sdf.glsl.h"
+
+#include <xhl/maths.h>
+#include <xhl/time.h>
+
+static struct
+{
+    int window_width;
+    int window_height;
+
+    float mouse_xy[2];
+
+    sg_buffer      buf;
+    sg_pipeline    pip;
+    sg_bindings    bind;
+    sg_pass_action pass_action;
+} state = {0};
+
+typedef struct
+{
+    float   x, y;
+    int16_t u, v;
+} vertex_t;
+
+bool         init_buffer = false;
+static float AUDIO_BUFFER[2048];
+
+void program_setup()
+{
+    xtime_init();
+
+    state.window_width  = APP_WIDTH;
+    state.window_height = APP_HEIGHT;
+
+    state.mouse_xy[0] = 1;
+    state.mouse_xy[1] = 0.5;
+
+    state.pip = sg_make_pipeline(&(
+        sg_pipeline_desc){.shader = sg_make_shader(line_stroke_shader_desc(sg_query_backend())), .label = "pipeline"});
+
+    state.pass_action =
+        (sg_pass_action){.colors[0] = {.load_action = SG_LOADACTION_CLEAR, .clear_value = {0.0f, 0.0f, 0.0f, 1.0f}}};
+
+    state.buf = sg_make_buffer(&(sg_buffer_desc){
+        .usage.storage_buffer = true,
+        .usage.stream_update  = true,
+        .size                 = sizeof(AUDIO_BUFFER),
+        .label                = "sine-buffer",
+    });
+
+    state.bind.views[VIEW_storage_buffer] = sg_make_view(&(sg_view_desc){.storage_buffer = state.buf});
+}
+void program_shutdown() {}
+
+bool program_event(const PWEvent* e)
+{
+    if (e->type == PW_EVENT_RESIZE_UPDATE)
+    {
+        state.window_width  = e->resize.width;
+        state.window_height = e->resize.height;
+        init_buffer         = false;
+    }
+    else if (e->type == PW_EVENT_MOUSE_MOVE || e->type == PW_EVENT_MOUSE_ENTER || e->type == PW_EVENT_MOUSE_EXIT)
+    {
+        state.mouse_xy[0] = e->mouse.x / (float)state.window_width;
+        state.mouse_xy[1] = e->mouse.y / (float)state.window_height;
+    }
+    return false;
+}
+
+void program_tick()
+{
+    sg_begin_pass(&(sg_pass){.action = state.pass_action, .swapchain = get_swapchain(SG_PIXELFORMAT_RGBA8)});
+    const size_t N = state.window_width;
+
+    // if (init_buffer == false)
+    // {
+    //     init_buffer = true;
+
+    const size_t job_len = xm_minull(ARRLEN(AUDIO_BUFFER), N + 1);
+
+    // Animated sine wave
+    // double now_sec = 0;
+    double now_sec  = xtime_convert_ns_to_sec(xtime_now_ns());
+    now_sec        *= 0.125 * 0.25; // 0.25hz
+
+    const size_t buflen = ARRLEN(AUDIO_BUFFER);
+    const float  phase  = (now_sec - (int)now_sec) * XM_TAUf;
+    const float  inc    = XM_TAUf / (float)buflen * 8;
+    for (unsigned i = 0; i < job_len; i++)
+    {
+        AUDIO_BUFFER[i] = sinf(phase + i * inc); // sin
+        // AUDIO_BUFFER[i] = (((i >> 4) & 3) >> 1) ? -1 : 1; // square
+    }
+
+    sg_update_buffer(state.buf, &(sg_range){.ptr = AUDIO_BUFFER, sizeof(AUDIO_BUFFER[0]) * job_len});
+
+    sg_apply_pipeline(state.pip);
+    sg_apply_bindings(&state.bind);
+
+    const fs_uniforms_t uniforms = {
+        .u_mouse_xy[0]   = state.mouse_xy[0],
+        .u_mouse_xy[1]   = state.mouse_xy[1],
+        .u_view_size[0]  = state.window_width,
+        .u_view_size[1]  = state.window_height,
+        .u_stroke_width  = 2.0f,
+        .u_buffer_length = ARRLEN(AUDIO_BUFFER),
+    };
+    sg_apply_uniforms(UB_fs_uniforms, &SG_RANGE(uniforms));
+
+    sg_draw(0, 6, 1);
+    sg_end_pass();
+}
---
+++ src/program_line_stroke_sdf.glsl
@@ -0,0 +1,83 @@
+@vs vs
+const vec2 positions[3] = { vec2(-1, -1), vec2(3, -1), vec2(-1, 3), };
+
+out vec2 uv;
+
+void main() {
+    vec2 pos = positions[gl_VertexIndex];
+    gl_Position = vec4(pos, 0, 1);
+    uv = pos * 0.5 + 0.5;
+}
+@end
+
+@fs fs
+
+in vec2 uv;
+out vec4 frag_colour;
+
+layout(binding=0) uniform fs_uniforms {
+    vec2  u_mouse_xy;
+    vec2  u_view_size;
+    float u_stroke_width;
+    int   u_buffer_length;
+};
+
+struct buffer_item {
+    float y;
+};
+
+layout(binding=0)readonly buffer storage_buffer {
+    buffer_item sine_buffer[];
+};
+
+float sdSegment(in vec2 p, in vec2 a, in vec2 b)
+{
+    vec2  ba = b - a;
+    vec2  pa = p - a;
+    float h  = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
+    return length(pa - h * ba);
+}
+
+void main() {
+    // Read buffer data
+
+    // If we pad the storage buffer by 1 on each side with real values, then we can get nicer looing clipped edges
+    uint idx      = min(int(uv.x * u_view_size.x),     u_buffer_length - 1);
+    uint idx_prev = max(int(uv.x * u_view_size.x) - 1, 0);
+    uint idx_next = min(int(uv.x * u_view_size.x) + 1, u_buffer_length - 1);
+
+    float stroke_width = u_stroke_width / u_view_size.y;
+
+    // float y_scale = 1.0;
+    // float y_scale = 1.0 - stroke_width;
+    float y_scale = 0.8;
+    float sine_y      = sine_buffer[idx].y * y_scale;
+    float sine_y_prev = sine_buffer[idx_prev].y * y_scale;
+    float sine_y_next = sine_buffer[idx_next].y * y_scale;
+
+
+    // build points
+    float px_inc = 2.0 / float(u_buffer_length);
+
+    vec2 p = uv * 2 - 1;
+    vec2 a = vec2(p.x - px_inc, sine_y_prev);
+    vec2 b = vec2(p.x         , sine_y);
+    vec2 c = vec2(p.x + px_inc, sine_y_next);
+
+    float d1 = sdSegment(p, a, b);
+    float d2 = sdSegment(p, b, c);
+    float d = min(d1, d2);
+
+    vec3 col_bg   = vec3(0);
+    vec3 col_line = vec3(1.0);
+
+    float shape_vertical   = smoothstep(stroke_width, 0, abs(d));
+    float shape_horizontal = smoothstep(stroke_width * 2, 0, abs(sine_y - p.y));
+    float shape            = max(shape_vertical, shape_horizontal);
+
+    vec3 col = mix(col_bg, col_line, shape);
+    frag_colour = vec4(col, 1.0);
+}
+@end
+
+@program line_stroke vs fs
