ce8442f3a3fefd8ced7cb073f61efa606f521e2f
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2026-01-18 12:49:47 +1000

    compile with VS

---
+++ generate_vs.bat
@@ -0,0 +1,4 @@
+mkdir .vs
+cmake -G "Visual Studio 17 2022" -B .vs
+
+cmd /k
--- src/main.c
+++ src/main.c
@@ -128,29 +128,29 @@ void* cplug_createPlugin(CplugHostContext* ctx)
     static int data = 0;
     return &data;
 }
-void cplug_destroyPlugin(void*) {}
+void cplug_destroyPlugin(void* _p) {}
 
-uint32_t cplug_getNumInputBusses(void*) { return 0; }
-uint32_t cplug_getNumOutputBusses(void*) { return 0; }
-uint32_t cplug_getInputBusChannelCount(void*, uint32_t bus_idx) { return 2; }
-uint32_t cplug_getOutputBusChannelCount(void*, uint32_t bus_idx) { return 2; }
-void     cplug_getInputBusName(void*, uint32_t idx, char* buf, size_t buflen) {}
-void     cplug_getOutputBusName(void*, uint32_t idx, char* buf, size_t buflen) {}
-uint32_t cplug_getLatencyInSamples(void*) { return 0; }
-uint32_t cplug_getTailInSamples(void*) { return 0; }
-void     cplug_setSampleRateAndBlockSize(void*, double sampleRate, uint32_t maxBlockSize) {}
+uint32_t cplug_getNumInputBusses(void*  _p) { return 0; }
+uint32_t cplug_getNumOutputBusses(void* _p) { return 0; }
+uint32_t cplug_getInputBusChannelCount(void* _p, uint32_t bus_idx) { return 2; }
+uint32_t cplug_getOutputBusChannelCount(void* _p, uint32_t bus_idx) { return 2; }
+void     cplug_getInputBusName(void* _p, uint32_t idx, char* buf, size_t buflen) {}
+void     cplug_getOutputBusName(void* _p, uint32_t idx, char* buf, size_t buflen) {}
+uint32_t cplug_getLatencyInSamples(void* _p) { return 0; }
+uint32_t cplug_getTailInSamples(void* _p) { return 0; }
+void     cplug_setSampleRateAndBlockSize(void* _p, double sampleRate, uint32_t maxBlockSize) {}
 void     cplug_process(void* userPlugin, CplugProcessContext* ctx) {}
-uint32_t cplug_getNumParameters(void*) { return 0; }
-uint32_t cplug_getParameterID(void*, uint32_t paramIndex) { return 0; }
-uint32_t cplug_getParameterFlags(void*, uint32_t paramId) { return 0; }
-void     cplug_getParameterRange(void*, uint32_t paramId, double* min, double* max) {}
-void     cplug_getParameterName(void*, uint32_t paramId, char* buf, size_t buflen) {}
-double   cplug_getParameterValue(void*, uint32_t paramId) { return 0; }
-double   cplug_getDefaultParameterValue(void*, uint32_t paramId) { return 0; }
-void     cplug_setParameterValue(void*, uint32_t paramId, double value) {}
-double   cplug_denormaliseParameterValue(void*, uint32_t paramId, double value) { return 0; }
-double   cplug_normaliseParameterValue(void*, uint32_t paramId, double value) { return 0; }
-double   cplug_parameterStringToValue(void*, uint32_t paramId, const char*) { return 0; }
-void     cplug_parameterValueToString(void*, uint32_t paramId, char* buf, size_t bufsize, double value) {}
+uint32_t cplug_getNumParameters(void* _p) { return 0; }
+uint32_t cplug_getParameterID(void* _p, uint32_t paramIndex) { return 0; }
+uint32_t cplug_getParameterFlags(void* _p, uint32_t paramId) { return 0; }
+void     cplug_getParameterRange(void* _p, uint32_t paramId, double* min, double* max) {}
+void     cplug_getParameterName(void* _p, uint32_t paramId, char* buf, size_t buflen) {}
+double   cplug_getParameterValue(void* _p, uint32_t paramId) { return 0; }
+double   cplug_getDefaultParameterValue(void* _p, uint32_t paramId) { return 0; }
+void     cplug_setParameterValue(void* _p, uint32_t paramId, double value) {}
+double   cplug_denormaliseParameterValue(void* _p, uint32_t paramId, double value) { return 0; }
+double   cplug_normaliseParameterValue(void* _p, uint32_t paramId, double value) { return 0; }
+double   cplug_parameterStringToValue(void* _p, uint32_t paramId, const char* str) { return 0; }
+void     cplug_parameterValueToString(void* _p, uint32_t paramId, char* buf, size_t bufsize, double value) {}
 void     cplug_saveState(void* userPlugin, const void* stateCtx, cplug_writeProc writeProc) {}
 void     cplug_loadState(void* userPlugin, const void* stateCtx, cplug_readProc readProc) {}
