a59b647f08f9df650cba5c546ef795d1bcb58738
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2026-03-04 11:34:19 +1000

    Fix render quality issues

--- src/program_nanosvg.c
+++ src/program_nanosvg.c
@@ -1,5 +1,6 @@
 #define NANOSVG_IMPLEMENTATION
 #define NANOSVGRAST_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
 
 #include "common.h"
 
@@ -29,6 +30,16 @@ static struct
     int width, height;
 } state;
 
+static const sg_color_target_state BLEND_DEFAULT = {
+    .write_mask = SG_COLORMASK_RGBA,
+    .blend      = {
+             .enabled          = true,
+             .src_factor_rgb   = SG_BLENDFACTOR_SRC_ALPHA,
+             .src_factor_alpha = SG_BLENDFACTOR_ONE,
+             .dst_factor_rgb   = SG_BLENDFACTOR_ONE_MINUS_SRC_ALPHA,
+             .dst_factor_alpha = SG_BLENDFACTOR_ONE,
+    }};
+
 void program_setup()
 {
     xalloc_init();
@@ -49,9 +60,10 @@ void program_setup()
     }
 
     println("SVG size: %f x %f", state.svg->width, state.svg->height);
-    int            scale = 32;
-    int            w     = state.svg->width * scale;
-    int            h     = state.svg->height * scale;
+    float scale = APP_HEIGHT / state.svg->height;
+    // float          scale = 1;
+    int            w   = (int)ceilf(state.svg->width * scale);
+    int            h   = (int)ceilf(state.svg->height * scale);
     unsigned char* img = img = malloc(w * h * 4);
     println("IMG size (scaled): %d x %d", w, h);
 
@@ -60,7 +72,7 @@ void program_setup()
 
         nsvgRasterize(state.rast, state.svg, 0, 0, scale, img, w, h, w * 4);
 
-        xtime_stopwatch_log_ms("Raster image in:");
+        xtime_stopwatch_log_ms("Raster image in");
     }
 
     state.svg_img  = sg_make_image(&(sg_image_desc){
@@ -70,18 +82,20 @@ void program_setup()
          .data.mip_levels[0] = {
              .ptr  = img,
              .size = w * h * 4,
-
         }});
     state.svg_view = sg_make_view(&(sg_view_desc){.texture = state.svg_img});
     state.svg_smp  = sg_make_sampler(&(sg_sampler_desc){
-         .min_filter = SG_FILTER_LINEAR,
-         .mag_filter = SG_FILTER_LINEAR,
-        //  .min_filter = SG_FILTER_NEAREST,
-        //  .mag_filter = SG_FILTER_NEAREST,
+         .min_filter    = SG_FILTER_NEAREST,
+         .mag_filter    = SG_FILTER_NEAREST,
+         .mipmap_filter = SG_FILTER_NEAREST,
+         .wrap_u        = SG_WRAP_CLAMP_TO_EDGE,
+         .wrap_v        = SG_WRAP_CLAMP_TO_EDGE,
     });
 
-    state.pip =
-        sg_make_pipeline(&(sg_pipeline_desc){.shader = sg_make_shader(nanosvg_shader_desc(sg_query_backend()))});
+    state.pip = sg_make_pipeline(&(sg_pipeline_desc){
+        .shader    = sg_make_shader(nanosvg_shader_desc(sg_query_backend())),
+        .colors[0] = BLEND_DEFAULT,
+    });
 
     free(img);
 }
@@ -118,6 +132,19 @@ void program_tick()
         .views[VIEW_tex]   = state.svg_view,
         .samplers[SMP_smp] = state.svg_smp,
     });
-    sg_draw(0, 3, 1);
+
+    sg_image_desc img_desc = sg_query_image_desc(state.svg_img);
+
+    vs_uniforms_t uniforms = {
+        .topleft     = {0, 0},
+        .bottomright = {img_desc.width, img_desc.height},
+        .size        = {state.width, state.height},
+
+        .img_topleft     = {0, 0},
+        .img_bottomright = {img_desc.width, img_desc.height},
+    };
+    sg_apply_uniforms(UB_vs_uniforms, &SG_RANGE(uniforms));
+
+    sg_draw(0, 6, 1);
     sg_end_pass();
 }
--- src/program_nanosvg.glsl
+++ src/program_nanosvg.glsl
@@ -1,12 +1,36 @@
 @vs vs
-const vec2 positions[3] = { vec2(-1, -1), vec2(3, -1), vec2(-1, 3), };
+layout(binding = 0) uniform vs_uniforms {
+    vec2 topleft;
+    vec2 bottomright;
+    vec2 size;
 
-out vec2 uv;
+    vec2 img_topleft;
+    vec2 img_bottomright;
+};
+
+out vec2 texcoord;
 
 void main() {
-    vec2 pos = positions[gl_VertexIndex];
-    gl_Position = vec4(pos, 0, 1);
-    uv = (pos * vec2(1, -1) + 1) * 0.5;
+    uint v_idx = gl_VertexIndex / 6u;
+    uint i_idx = gl_VertexIndex - v_idx * 6;
+
+    // Is odd
+    bool is_right = (gl_VertexIndex & 1) == 1;
+    bool is_bottom = i_idx >= 2 && i_idx <= 4;
+
+    vec2 pos = vec2(
+        is_right  ? bottomright.x : topleft.x,
+        is_bottom ? bottomright.y : topleft.y
+    );
+    pos = (pos + pos) / size - vec2(1);
+    pos.y = -pos.y;
+
+    gl_Position = vec4(pos, 1, 1);
+
+    texcoord = vec2(
+        is_right  ? img_bottomright.x : img_topleft.x,
+        is_bottom ? img_bottomright.y : img_topleft.y
+    );
 }
 @end
 
@@ -14,11 +38,17 @@ void main() {
 layout(binding = 0) uniform texture2D tex;
 layout(binding = 0) uniform sampler smp;
 
-in vec2 uv;
+in vec2 texcoord;
 out vec4 frag_color;
 
 void main() {
-    frag_color = texture(sampler2D(tex, smp), uv);
+
+    ivec2 itexcoord = ivec2(texcoord);
+    frag_color = texelFetch(sampler2D(tex, smp), itexcoord, 0);
+
+
+    // frag_color = texture(sampler2D(tex, smp), uv);
+    // frag_color += vec4(uv, 1, 1);
 }
 @end
 
